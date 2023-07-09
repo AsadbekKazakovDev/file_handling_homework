9c3b7aa7a3e4007d37cc542205c930c486b2f31c
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-09 22:49:50 +0500

    solved 4

--- file04.py
+++ file04.py
@@ -1,3 +1,4 @@
+import re
 def main(data:str):
     """
     The data is from the file. Return the str(non-digital) characters as a list type.
@@ -6,5 +7,18 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    result = []
+    non_digit_characters = re.findall(r'\D', data)
+
+        # Add each non-digit character to the result list
+    for character in non_digit_characters:
+        result.append(character)
+
+    return result
+f = open("data/data04.txt","r")
+data  = f.read()
+f.close
+
+print(main(data))
     
 # Read data from file

bc883ea1f5b639f6d07e76b32f633b84c49cd806
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-09 23:31:11 +0500

    solved 6

--- file06.py
+++ file06.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    result  = []
+    rows = data.split("\n")
+    for row in rows:
+        result.append(len(row))
+    return result
+f = open("data/data06.txt","r")
+data  = f.read()
+f.close
+
+print(main(data))
+# Read data from file
 # Read data from file

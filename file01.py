17080744601e0d0c8523a819eb9dd0ca9e5368c2
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-09 22:36:59 +0500

    solved 1

--- file01.py
+++ file01.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    result = []
+    elements = data.split(",")
+    for element in elements:
+        result.append(int(element))
+    return result
+f = open("data/data01.txt","r")
+data  = f.read()
+f.close
+k  = main(data)
+print(k)   
 # Read data from file

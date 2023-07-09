5a658924b0f18cd37a04f87ac812a82ea1b26708
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-09 22:52:48 +0500

    solved 5

--- file05.py
+++ file05.py
@@ -1,3 +1,4 @@
+import re
 def main(data:str):
     """
     The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
@@ -6,5 +7,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    digits = re.findall(r'\d', data)
+    digit_count = len(digits)
+
+        # Count the number of non-digit characters
+    non_digits = re.findall(r'\D', data)
+    non_digit_count = len(non_digits)
+
+    return [digit_count, non_digit_count]
+f = open("data/data05.txt","r")
+data  = f.read()
+f.close
+
+print(main(data))
 # Read data from file

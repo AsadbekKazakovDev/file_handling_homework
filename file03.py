c062f1a8d0e6b1e8463a851e3d3c72acf51bd8a2
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-09 22:44:54 +0500

    Return the str(non-digital) characters as a list type.

--- file03.py
+++ file03.py
@@ -1,3 +1,4 @@
+import re
 def main(data:str):
     """
     The data is from the file. Return the digits as a list type.
@@ -6,5 +7,19 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    a  = "0123456789"
+    result = []
+    numbers = re.findall(r'\d', data)
+        
+        # Convert each number to a string and store them in a list
+    for number in numbers:
+        result.append(number)
+    
+    return result  
+f = open("data/data03.txt","r")
+data  = f.read()
+f.close
+
+print(main(data))
 
 # Read data from file

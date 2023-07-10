b09ec097ec72ebe9dfc8db30122ad3f1f837362d
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-10 06:50:06 +0500

    solved 8

--- file08.py
+++ file08.py
@@ -1,3 +1,4 @@
+import re
 def main(data:str):
     """
     The data is from the file. Find the largest of the numeric characters.
@@ -6,5 +7,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    digits = [int(digit) for digit in data if digit.isdigit()]
+    if digits:
+        return max(digits)
+f = open("data/data08.txt","r")
+data  = f.read()
+f.close
+
+print(main(data))
+    
 
 # Read data from file

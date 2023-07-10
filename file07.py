d48df5d9c036dc0bc0130c0487ea148988ba2685
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-10 06:42:26 +0500

    solved 7

--- file07.py
+++ file07.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    result  = 0
+    #rows = data.split("\n")
+    for row in range(len(data)):
+        if data[row].isdigit():
+            result+=int(data[row])
+    return result
+f = open("data/data07.txt","r")
+data  = f.read()
+f.close
+
+print(main(data))
     
 # Read data from file

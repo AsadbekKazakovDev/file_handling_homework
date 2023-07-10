cb142053b1109c2aa5d4f631d1f8da9ae7545b22
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-10 07:09:20 +0500

    solved 10

--- file10.py
+++ file10.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    largest_length = 0
+    rows = data.split("\n")
+    for row in rows:
+        row_length = len(row.strip())
+        if row_length > largest_length:
+            largest_length = row_length
+    
+    return largest_length
+f = open("data/data10.txt","r")
+data  = f.read()
+f.close
 
+print(main(data))
 # Read data from file
